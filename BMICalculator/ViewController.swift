7a70fb8ff3637223e560c79d7ce10fcccc6248cf
chaewon-moana <dreamytaurus508@gmail.com>
2024-01-04 19:50:53 +0900

    nickname 추가 BMI 계산기 적용

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var mainLabel: UILabel!
     @IBOutlet var subLabel: UILabel!
+    @IBOutlet var resetLabel: UILabel!
+    
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var nicknameTextField: UITextField!
     
     @IBOutlet var heightLabel: UILabel!
     @IBOutlet var heightTextField: UITextField!
@@ -21,21 +25,28 @@ class ViewController: UIViewController {
     
     @IBOutlet var randomLabel: UILabel!
     @IBOutlet var resultButton: UIButton!
+    @IBOutlet var beforeValue: UIButton!
+    
+    
+    //reset 버튼 누르면 초기화한다고 alert창 띄우기
+    //UserDefaults로 키, 몸무게, 닉네임 저장하기
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setTextField(nicknameTextField, text: "대소문자 구분이 없어요")
         setTextField(heightTextField, text: "cm단위로 입력해주세요")
         setTextField(weightTextField, text: "kg단위로 입력해주세요")
         
+        setLabel(nicknameLabel, text: "닉네임을 입력해주세요")
         setLabel(heightLabel, text: "키가 어떻게 되시나요?")
         setLabel(weightLabel, text: "몸무게는 어떻게 되시나요?")
         
-        
         setMainUI()
-        setResultButton(resultButton)
+        setResultButton(resultButton, text: "결과 확인", color: .systemPurple)
+        setResultButton(beforeValue, text: "이전 값 호출", color: .systemBlue)
         
-        
+        nicknameTextField.text = UserDefaults.standard.string(forKey: "nickname") ?? nil
     }
     
     func setMainUI() {
@@ -49,12 +60,17 @@ class ViewController: UIViewController {
         subLabel.textAlignment = .left
         subLabel.font = .systemFont(ofSize: 14)
         
+        resetLabel.text = "값 초기화"
+        resetLabel.textAlignment = .right
+        resetLabel.textColor = .systemRed
+        resetLabel.font = .systemFont(ofSize: 13)
+        resetLabel.isUserInteractionEnabled = true
+        
         randomLabel.text = "랜덤으로 BMI 계산하기"
         randomLabel.textAlignment = .right
-        randomLabel.textColor = .systemRed
+        randomLabel.textColor = .systemBlue
         randomLabel.font = .systemFont(ofSize: 13)
         randomLabel.isUserInteractionEnabled = true
-        
     }
     
    
@@ -96,19 +112,17 @@ class ViewController: UIViewController {
         label.font = .systemFont(ofSize: 16)
     }
     
-    func setResultButton(_ button: UIButton) {
-        button.setTitle("결과 확인", for: .normal)
+    func setResultButton(_ button: UIButton, text: String, color: UIColor) {
+        button.setTitle(text, for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 18)
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemPurple
+        button.backgroundColor = color
         button.layer.cornerRadius = 14
-        
-        
     }
+  
     
     
     @IBAction func resultButtonTapped(_ sender: UIButton) {
-
         
         guard let height = Int(heightTextField.text!) else {
             print("키에서 오류")
@@ -118,6 +132,13 @@ class ViewController: UIViewController {
             print("몸무게에서 오류")
             return
         }
+        
+        guard let nickname = nicknameTextField.text else {
+            print("이름 오류")
+            return
+        }
+        
+        let name = nickname.lowercased()
         
         print(height, weight)
         
@@ -135,16 +156,21 @@ class ViewController: UIViewController {
         warning.addAction(button2)
         
         if height >= 50 && height <= 300 && weight >= 30 && weight <= 300 {
+            UserDefaults.standard.setValue(name, forKey: "nickname")
+            UserDefaults.standard.setValue(height, forKey: "height")
+            UserDefaults.standard.setValue(weight, forKey: "weight")
             present(alert, animated: true)
         } else {
             present(warning, animated: true)
         }
-        
     }
     
     
+    @IBAction func nicknameInputTextField(_ sender: UITextField) {
+        sender.text = sender.text?.lowercased()
+    }
+    
     @IBAction func heightInputTextField(_ sender: UITextField) {
-        
         //문자 입력했을 때, 오류 처리
         //공백이 빈칸 처리,,
         //범위가 이상한 경우 처리
@@ -158,8 +184,6 @@ class ViewController: UIViewController {
             heightLabel.text = "다시입력해주세요"
             return
         }
-        
-        
         sender.text = String(height)
     }
     
@@ -181,17 +205,38 @@ class ViewController: UIViewController {
         sender.text = String(weight)
     }
     
- 
+    @IBAction func resetButtonTapped(_ sender: UITapGestureRecognizer) {
+        UserDefaults.standard.set(nil, forKey: "nickname")
+        UserDefaults.standard.set(nil, forKey: "height")
+        UserDefaults.standard.set(nil, forKey: "weight")
+        
+        nicknameTextField.text = nil
+        heightTextField.text = nil
+        weightTextField.text = nil
+    }
+    
+    
     @IBAction func randomBMI(_ sender: UITapGestureRecognizer) {
         heightTextField.text = String(Int.random(in: 50...300))
         weightTextField.text = String(Int.random(in: 30...300))
     }
     
     
+    @IBAction func beforeValueTapped(_ sender: UIButton) {
+        nicknameTextField.text = UserDefaults.standard.string(forKey: "nickname")
+        heightTextField.text = String(UserDefaults.standard.integer(forKey: "height"))
+        weightTextField.text = String(UserDefaults.standard.integer(forKey: "weight"))
+    }
+    
+    
+    
     @IBAction func keyboardDismiss(_ sender: UITextField) {
     }
     
     @IBAction func keyboardDismissTwo(_ sender: UITextField) {
+    }
+    
+    @IBAction func keyboardDismissThree(_ sender: UITextField) {
     }
     
     
